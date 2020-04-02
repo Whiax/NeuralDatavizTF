7a66204d2b7c7e4abde151e787bf44401c19c42b
Whiax <ragnaarok@hotmail.fr>
2020-04-03 01:07:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -47,9 +47,7 @@ biases = {
 
 # Create model
 def multilayer_perceptron(x):
-    # Hidden fully connected layer with 256 neurons
     layer_1 = tf.add(tf.nn.leaky_relu(tf.matmul(x, weights['h1'])), biases['b1'])
-    # Hidden fully connected layer with 256 neurons
     layer_2 = tf.add(tf.nn.leaky_relu(tf.matmul(layer_1, weights['h2'])), biases['b2'])
     # Output fully connected layer with a neuron for each class
     out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
